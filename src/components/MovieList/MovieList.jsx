480f54b408d045d5078072d081b6ea3bbab8db54
Anastasiia Tarasiuk <111896564+anastasiiatarasiuk@users.noreply.github.com>
2024-07-29 10:46:32 +0300

    add useLocation

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,8 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import s from "./MovieList.module.css";
 import { BASE_POSTER_URL } from "../../services/getMovies";
 
 const MovieList = ({ movies = [] }) => {
+  const location = useLocation();
   const defaultImg =
     "<https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg>";
   return (
@@ -10,7 +11,11 @@ const MovieList = ({ movies = [] }) => {
       <ul className={s.list}>
         {movies.map((movie) => (
           <li key={movie.id} className={s.items}>
-            <Link className={s.link} to={`/movies/${movie.id}`}>
+            <Link
+              className={s.link}
+              to={`/movies/${movie.id}`}
+              state={location}
+            >
               <img
                 src={
                   movie.poster_path
